ffadff59d3faeca7dd6815d35e948e64230a5da2
Abhishek Raj <abhishekraj272@gmail.com>
2021-03-31 22:25:57 +0530

    game working now

--- game.go
+++ game.go
@@ -150,28 +150,28 @@ func (g *Game) moveUp() {
 	g.headX--
 	g.board[g.headX][g.headY] = 1
 	g.board[g.tailX][g.tailY] = 0
-	// g.getTail()
+	g.getTail()
 }
 
 func (g *Game) moveRight() {
 	g.headY++
 	g.board[g.headX][g.headY] = 1
 	g.board[g.tailX][g.tailY] = 0
-	// g.getTail()
+	g.getTail()
 }
 
 func (g *Game) moveDown() {
 	g.headX++
 	g.board[g.headX][g.headY] = 1
 	g.board[g.tailX][g.tailY] = 0
-	// g.getTail()
+	g.getTail()
 }
 
 func (g *Game) moveLeft() {
 	g.headY--
 	g.board[g.headX][g.headY] = 1
 	g.board[g.tailX][g.tailY] = 0
-	// g.getTail()
+	g.getTail()
 }
 
 func (g *Game) getTail() {
@@ -181,17 +181,24 @@ func (g *Game) getTail() {
 		return
 	}
 
-	if g.tailX != 0 || g.tailX != g.boardHeight {
-		if g.board[g.tailX-1][g.tailY] == 1 {
-			g.tailX--
-		} else if g.board[g.tailX+1][g.tailY] == 1 {
-			g.tailX++
-		}
-	} else if g.tailY != 0 || g.tailY != g.boardWidth {
-		if g.board[g.tailX][g.tailY-1] == 1 {
-			g.tailY--
-		} else {
-			g.tailY++
-		}
+	if g.tailX != 0 && g.board[g.tailX-1][g.tailY] == 1 {
+		g.tailX--
+		return
 	}
+
+	if g.tailX != g.boardHeight-1 && g.board[g.tailX+1][g.tailY] == 1 {
+		g.tailX++
+		return
+	}
+
+	if g.tailY != 0 && g.board[g.tailX][g.tailY-1] == 1 {
+		g.tailY--
+		return
+	}
+
+	if g.tailY != g.boardWidth-1 && g.board[g.tailX][g.tailY+1] == 1 {
+		g.tailY++
+		return
+	}
+
 }
